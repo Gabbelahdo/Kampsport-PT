2758b0976d1b8159cfb6e32559be109bdb00f329
Gabbelahdo <156900826+Gabbelahdo@users.noreply.github.com>
2026-02-26 21:34:31 +0100

    n.

--- backend/db.js
+++ backend/db.js
@@ -12,7 +12,8 @@ const databas = mysql.createPool({
   host: process.env.RDS_HOSTNAME,
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_DB_NAME
+  database: process.env.RDS_DB_NAME,
+  port: process.env.RDS_PORT
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const cors = require("cors");
-
+require('dotenv').config();
 const authRoutes = require('./routes/auth');
 const articlesRoutes = require('./routes/articles');
 const signupRoutes = require('./routes/signup');
 
-require('dotenv').config();
-
 const app = express();
-app.use(cors());
+app.use(cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type','Authorization'],
+        credentials: false
+}
+));
 app.use(express.json());
+
+
 
 app.use('/auth', authRoutes);
 app.use('/articles', articlesRoutes);
