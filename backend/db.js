174724cb55bfffd9cad15ef0cdda02dac176f628
Gabbelahdo <156900826+Gabbelahdo@users.noreply.github.com>
2026-02-26 11:59:15 +0100

    ändrade miljövariabler

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const databas = mysql.createPool({
+/*const databas = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
+});*/
+
+const databas = mysql.createPool({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DB_NAME
 });
+
 
 module.exports = databas;
